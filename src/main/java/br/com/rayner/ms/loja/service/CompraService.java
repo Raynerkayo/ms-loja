e1132dbd582b744af98d96e7857281811aadc767
kayo rayner <rayner.kayo.tec@gmail.com>
2020-05-23 19:59:11 -0300

    Correções

--- src/main/java/br/com/rayner/ms/loja/service/CompraService.java
+++ src/main/java/br/com/rayner/ms/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.rayner.ms.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,14 @@ import br.com.rayner.ms.loja.model.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public void realizarCompra(CompraDTO compra) {
 
-		RestTemplate client = new RestTemplate();
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://localhost:8081/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
+		ResponseEntity<InfoFornecedorDTO> exchange = restTemplate.exchange(
+				"http://fornecedor/fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
 
 		System.out.println(exchange.getBody().getEndereco());
