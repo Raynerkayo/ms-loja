c66e1d640da0854b4b2807225be33553e0b70fa1
kayo rayner <rayner.kayo.tec@gmail.com>
2020-05-24 13:01:40 -0300

    Usando o FeignClient para as requisições aos outros MS.

--- src/main/java/br/com/rayner/ms/loja/service/CompraService.java
+++ src/main/java/br/com/rayner/ms/loja/service/CompraService.java
@@ -1,28 +1,23 @@
 package br.com.rayner.ms.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.rayner.ms.loja.client.FornecedorClient;
 import br.com.rayner.ms.loja.controller.dto.CompraDTO;
 import br.com.rayner.ms.loja.model.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
-	
+
 	@Autowired
-	private RestTemplate restTemplate;
+	private FornecedorClient client;
 
 	public void realizarCompra(CompraDTO compra) {
 
-		ResponseEntity<InfoFornecedorDTO> exchange = restTemplate.exchange(
-				"http://fornecedor/fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
-				InfoFornecedorDTO.class);
+		InfoFornecedorDTO infoPorEstado = client.getInfoPorEstado(compra.getEndereco().getEstado());
 
-		System.out.println(exchange.getBody().getEndereco());
-		
+		System.out.println(infoPorEstado.getEndereco());
 	}
 
 }
