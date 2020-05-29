729e1d0ccc3e09baeb2bfa28eb7d0f0edd4b1c4c
kpinheirors <kpinheiro@cpqi.com>
2020-05-29 16:22:08 -0300

    Adicionando serviço externo para salvar os LOGs.

--- src/main/java/br/com/rayner/ms/loja/service/CompraService.java
+++ src/main/java/br/com/rayner/ms/loja/service/CompraService.java
@@ -1,10 +1,9 @@
 package br.com.rayner.ms.loja.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.rayner.ms.loja.client.FornecedorClient;
 import br.com.rayner.ms.loja.controller.dto.CompraDTO;
@@ -27,7 +26,7 @@ public class CompraService {
 		LOG.info("Buscando informações de fornecedores do estado do: {}", estado);
 		InfoFornecedorDTO infoPorEstado = client.getInfoPorEstado(estado);
 
-		LOG.info("Realizando um pedido.");
+		LOG.info("Realizando um pedido." + infoPorEstado);
 		InfoPedidoDTO pedido = client.realizaPedido(compra.getItens());
 
 		Compra compraSalva = new Compra();
